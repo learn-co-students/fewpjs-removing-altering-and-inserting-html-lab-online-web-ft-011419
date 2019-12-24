546da706322be1df171bd8ec17aa5dded2002700
Simcha Greenbaum <gbs4ever2@gmail.com>
2019-12-24 01:11:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.querySelector("main#main").remove();
+let newHeader = document.createElement('h1')
+newHeader.id = "victory"
+newHeader.innerHTML ="YOUR-NAME is the champion"
+// 'newHeader' variable that points to node 'h1#victory'
+// 'newHeader' variable that points to node 'h1#victory' with "YOUR-NAME is the champion" inside
